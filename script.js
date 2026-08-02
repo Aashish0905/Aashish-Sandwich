f078b3dd30909aa76739774d5d3e11812c4e213a
Aashish Raikwar <166126347+Aashish0905@users.noreply.github.com>
2026-08-03 00:43:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+//  <!-- NAVBAR SECTION START -->
+
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+//  <!-- NAVBAR SECTION END  -->
+
+
+// EmailJS Initialize
+emailjs.init("YOUR_PUBLIC_KEY");
+
+const contactForm = document.getElementById("contact-form");
+
+contactForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    emailjs.sendForm(
+        "YOUR_SERVICE_ID",
+        "YOUR_TEMPLATE_ID",
+        this
+    )
+    .then(() => {
+        alert("Message Sent Successfully!");
+        contactForm.reset();
+    })
+    .catch((error) => {
+        alert("Failed To Send Message");
+        console.error(error);
+    });
+});
